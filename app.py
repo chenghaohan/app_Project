98291b3252933452d575fa86df08344ab9f390f3
Heather Dietrich <71471631+HDietrich416@users.noreply.github.com>
2021-02-02 21:40:25 -0500

    connection complete

--- app.py
+++ app.py
@@ -5,19 +5,34 @@ from flask import (
     jsonify,
     request,
     redirect)
-from flask_sqlalchemy import SQLAlchemy
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import func
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://cggjytcd:2Lf6GkD0Cb8TbV6e4-X7ZBCvNMh_zV3F@raja.db.elephantsql.com:5432/cggjytcd"
-db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://cggjytcd:2Lf6GkD0Cb8TbV6e4-X7ZBCvNMh_zV3F@raja.db.elephantsql.com:5432/cggjytcd"
+#db = SQLAlchemy(app)
 
+# Create an engine for the chinook.sqlite database
+engine = create_engine("postgresql+psycopg2://cggjytcd:2Lf6GkD0Cb8TbV6e4-X7ZBCvNMh_zV3F@raja.db.elephantsql.com:5432/cggjytcd", echo=False)
 
+# Reflect Database into ORM classes
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+#Base.classes()
+print(Base.classes.keys())
+can_cases = Base.classes.can_cases
+
+session = Session(engine)
 
 ## FRONT_END ROUTES
 @app.route("/")
 def main(): 
-    return render_template("templates/index.html")
+    return render_template("index.html")
 
 
 ## SERVICE ROUTES
@@ -25,20 +40,26 @@ def main():
 ## Stock Prices
 @app.route("/api/main/stockdata")
 def firstRoute(): 
-    data = db.session.query(cisco, cnn_news, shopify, slack, zoom).all()
-    return jsonify(data)
+    
+    data = session.query(can_cases.No_Cases).all()
+    dataset = []
+    for item in data:
+        dataset.append(item)
+
+    output = { "output" : [dataset] }
+    return jsonify(output)
 
 ## Canadian Cases, Deaths, Vaccines by Province
-@app.route("/api/main/cancovid")
-def firstRoute(): 
-    data = db.session.query(can_cases, can_deaths, vaccine_admin, canadian_coordinates).all()
-    return jsonify(data)
+# @app.route("/api/main/cancovid")
+# def firstRoute(): 
+#     data = db.session.query(can_cases, can_deaths, vaccine_admin, canadian_coordinates).all()
+#     return jsonify(data)
 
-## Global Cases, Deaths
-@app.route("/api/main/globalcovid")
-def firstRoute(): 
-    data = db.session.query(global_cases, global_deaths, world_cordinates).all()
-    return jsonify(data)
+# ## Global Cases, Deaths
+# @app.route("/api/main/globalcovid")
+# def firstRoute(): 
+#     data = db.session.query(global_cases, global_deaths, world_cordinates).all()
+#     return jsonify(data)
 
 
 
