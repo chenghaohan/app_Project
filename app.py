4352a3412f93fa954bc6ea707a82276ad4e204c4
Heather Dietrich <71471631+HDietrich416@users.noreply.github.com>
2021-02-04 11:49:24 -0500

    created app route for Canadian COVID map

--- app.py
+++ app.py
@@ -23,9 +23,13 @@ engine = create_engine("postgresql+psycopg2://cggjytcd:2Lf6GkD0Cb8TbV6e4-X7ZBCvN
 # Reflect Database into ORM classes
 Base = automap_base()
 Base.prepare(engine, reflect=True)
-#Base.classes()
+
 print(Base.classes.keys())
 can_cases = Base.classes.can_cases
+can_deaths = Base.classes.can_deaths
+vaccine_admin = Base.classes.vaccine_admin
+canadian_coordinates = Base.classes.canadian_coordinates
+
 
 session = Session(engine)
 
@@ -50,10 +54,34 @@ def firstRoute():
     return jsonify(output)
 
 ## Canadian Cases, Deaths, Vaccines by Province
-# @app.route("/api/main/cancovid")
-# def firstRoute(): 
-#     data = db.session.query(can_cases, can_deaths, vaccine_admin, canadian_coordinates).all()
-#     return jsonify(data)
+@app.route("/api/main/cancovid")
+def cancovidRoute(): 
+    
+    cases_data = session.query(can_cases.Province, can_cases.Date_dt, can_cases.No_Cases).all()
+    cases_dataset = []
+    for item in cases_data:
+        cases_dataset.append(item)
+    
+    deaths_data = session.query(can_deaths.Province, can_deaths.Date_dt, can_deaths.No_Deaths).all()
+    deaths_dataset = []
+    for item in deaths_data:
+        deaths_dataset.append(item)
+
+    vaccine_data = session.query(vaccine_admin.Province, vaccine_admin.Date, vaccine_admin.Vaccine_Administered).all()
+    vaccine_dataset = []
+    for item in vaccine_data:
+        vaccine_dataset.append(item)
+    
+    can_coor_data = session.query(canadian_coordinates.Province, canadian_coordinates.Latitude, canadian_coordinates.Longitude).all()
+    can_coor_dataset = []
+    for item in can_coor_data:
+        can_coor_dataset.append(item)
+
+    output = { "Canadian_Cases" : [cases_dataset], 
+                "Canadian Deaths" : [deaths_dataset], 
+                "Canadian Vaccines Admin" : [vaccine_dataset], 
+                "Canadian Coordinates" : [can_coor_dataset]}
+    return jsonify(output)
 
 # ## Global Cases, Deaths
 # @app.route("/api/main/globalcovid")
