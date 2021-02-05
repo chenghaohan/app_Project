e5470a57bd639d6ec8419a19b29e9aa340923ee3
Heather Dietrich <71471631+HDietrich416@users.noreply.github.com>
2021-02-04 21:59:00 -0500

    update link to json data

--- covid_app/static/js/logic.js
+++ covid_app/static/js/logic.js
@@ -15,67 +15,68 @@ var myMap = L.map("map", {
   }).addTo(myMap);
   
   // Load in geojson data
-  var geoData = "https://covid19-dashboard2021.herokuapp.com/api/main/cancovid";
+  var geoData = "/api/main/cancovid";
+
   
   var geojson;
   
   // Grab data with d3
   d3.json(geoData, function(data) {
-  
+    console.log(data);
     // Create a new choropleth layer
-    geojson = L.choropleth(data, {
+    // geojson = L.choropleth(data, {
   
-      // Define what  property in the features to use
-      valueProperty: "Canadian_Cases",
+    //   // Define what  property in the features to use
+    //   valueProperty: "Canadian_Cases",
   
-      // Set color scale
-      scale: ["#ffffb2", "#b10026"],
+    //   // Set color scale
+    //   scale: ["#ffffb2", "#b10026"],
   
-      // Number of breaks in step range
-      steps: 10,
+    //   // Number of breaks in step range
+    //   steps: 10,
   
-      // q for quartile, e for equidistant, k for k-means
-      mode: "q",
-      style: {
-        // Border color
-        color: "#fff",
-        weight: 1,
-        fillOpacity: 0.8
-      },
+    //   // q for quartile, e for equidistant, k for k-means
+    //   mode: "q",
+    //   style: {
+    //     // Border color
+    //     color: "#fff",
+    //     weight: 1,
+    //     fillOpacity: 0.8
+    //   },
   
-      // Binding a pop-up to each layer
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup("Cases: " + feature.Canadian_Cases);
-      }
-    }).addTo(myMap);
+    //   // Binding a pop-up to each layer
+    //   onEachFeature: function(feature, layer) {
+    //     layer.bindPopup("Cases: " + feature.Canadian_Cases);
+    //   }
+    // }).addTo(myMap);
   
-    // Set up the legend
-    var legend = L.control({ position: "bottomright" });
-    legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "info legend");
-      var limits = geojson.options.limits;
-      var colors = geojson.options.colors;
-      var labels = [];
+    // // Set up the legend
+    // var legend = L.control({ position: "bottomright" });
+    // legend.onAdd = function() {
+    //   var div = L.DomUtil.create("div", "info legend");
+    //   var limits = geojson.options.limits;
+    //   var colors = geojson.options.colors;
+    //   var labels = [];
   
-      // Add min & max
-      var legendInfo = "<h1>Canadian Cases</h1>" +
-        "<div class=\"labels\">" +
-          "<div class=\"min\">" + limits[0] + "</div>" +
-          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-        "</div>";
+    //   // Add min & max
+    //   var legendInfo = "<h1>Canadian Cases</h1>" +
+    //     "<div class=\"labels\">" +
+    //       "<div class=\"min\">" + limits[0] + "</div>" +
+    //       "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+    //     "</div>";
   
-      div.innerHTML = legendInfo;
+    //   div.innerHTML = legendInfo;
   
-      limits.forEach(function(limit, index) {
-        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-      });
+    //   limits.forEach(function(limit, index) {
+    //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    //   });
   
-      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-      return div;
-    };
+    //   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    //   return div;
+    // };
   
-    // Adding legend to the map
-    legend.addTo(myMap);
+    // // Adding legend to the map
+    // legend.addTo(myMap);
   
   });
   
