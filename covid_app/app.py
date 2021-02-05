c827e7afdb424415931d02c8b27e47760954bbbd
chenghaohan <71295207+chenghaohan@users.noreply.github.com>
2021-02-04 22:09:42 -0500

    stock price final output

--- covid_app/app.py
+++ covid_app/app.py
@@ -49,7 +49,7 @@ def main():
 ## Stock Prices
 @app.route("/api/main/stockdata")
 def stockRoute(): 
-    
+    #pulling zoom prices from SQL server
     zoom_data = session.query(zoom.Date,zoom.Open,zoom.High,zoom.Low,zoom.Close,zoom.Volume).all()
     
     zoom_Date = []
@@ -83,7 +83,111 @@ def stockRoute():
                "zoom_Close" : zoom_Close,
                "zoom_Volume" : zoom_Vol
      }
-    return jsonify(zoom_output)
+
+
+    #pulling slack prices from SQL server
+    slack_data = session.query(slack.Date,slack.Open,slack.High,slack.Low,slack.Close,slack.Volume).all()
+    
+    slack_Date = []
+    for item in slack_data:
+        slack_Date.append(item[0])
+    
+    slack_Open = []
+    for item in slack_data:
+        slack_Open.append(item[1])
+    
+    slack_High = []
+    for item in slack_data:
+        slack_High.append(item[2])
+    
+    slack_Low = []
+    for item in slack_data:
+        slack_Low.append(item[3])
+    
+    slack_Close = []
+    for item in slack_data:
+        slack_Close.append(item[4])
+    
+    slack_Vol = []
+    for item in slack_data:
+        slack_Vol.append(item[5])
+
+    slack_output = { 
+               "slack_Date" : slack_Date,
+               "slack_Open" : slack_Open,
+               "slack_High" : slack_High,
+               "slack_Low" : slack_Low,
+               "slack_Close" : slack_Close,
+               "slack_Volume" : slack_Vol
+     }
+
+    #pulling cisco prices from SQL server
+    cisco_data = session.query(cisco.Date,cisco.Open,cisco.High,cisco.Low,cisco.Close,cisco.Volume).all()
+    
+    cisco_Date = []
+    for item in cisco_data:
+        cisco_Date.append(item[0])
+    
+    cisco_Open = []
+    for item in cisco_data:
+        cisco_Open.append(item[1])
+    
+    cisco_High = []
+    for item in cisco_data:
+        cisco_High.append(item[2])
+    
+    cisco_Low = []
+    for item in cisco_data:
+        cisco_Low.append(item[3])
+    
+    cisco_Close = []
+    for item in cisco_data:
+        cisco_Close.append(item[4])
+    
+    cisco_Vol = []
+    for item in cisco_data:
+        cisco_Vol.append(item[5])
+
+    cisco_output = { 
+               "cisco_Date" : cisco_Date,
+               "cisco_Open" : cisco_Open,
+               "cisco_High" : cisco_High,
+               "cisco_Low" : cisco_Low,
+               "cisco_Close" : cisco_Close,
+               "cisco_Volume" : cisco_Vol
+     }
+
+    #shopify data
+    shopify_data = session.query(shopify.Date,shopify.Open,shopify.High,shopify.Low,shopify.Close,shopify.Volume).all()
+    shopify_Date = []
+    for item in shopify_data:
+        shopify_Date.append(item[0])
+    shopify_Open = []
+    for item in shopify_data:
+        shopify_Open.append(item[1])
+    shopify_High = []
+    for item in shopify_data:
+        shopify_High.append(item[2])
+    shopify_Low = []
+    for item in shopify_data:
+        shopify_Low.append(item[3])
+    shopify_Close = []
+    for item in shopify_data:
+        shopify_Close.append(item[4])
+    shopify_Vol = []
+    for item in shopify_data:
+        shopify_Vol.append(item[5])
+    shopify_output = { "shopify_Date" : shopify_Date,
+               "shopify_Open" : shopify_Open,
+               "shopify_High" : shopify_High,
+               "shopify_Low" : shopify_Low,
+               "shopify_Close" : shopify_Close,
+               "shopify_Volume" : shopify_Vol
+     }
+    
+    final_output = []
+    final_output.extend((zoom_output,cisco_output,slack_output,shopify_output))
+    return jsonify(final_output)
 
 ## Canadian Cases, Deaths, Vaccines by Province
 @app.route("/api/main/cancovid")
