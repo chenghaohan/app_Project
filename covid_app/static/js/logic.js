bfd22668395bdcbe7528e76367c11082b720eb64
Heather Dietrich <71471631+HDietrich416@users.noreply.github.com>
2021-02-06 09:01:10 -0500

    added style.css for Canadian map and uncommented canadian map

--- covid_app/static/js/logic.js
+++ covid_app/static/js/logic.js
@@ -24,59 +24,59 @@ var myMap = L.map("map", {
   d3.json(geoData, function(data) {
     console.log(data);
     // Create a new choropleth layer
-    // geojson = L.choropleth(data, {
+    geojson = L.choropleth(data, {
   
-    //   // Define what  property in the features to use
-    //   valueProperty: "Canadian_Cases",
+    // Define what  property in the features to use
+    valueProperty: "Canadian_Cases",
   
-    //   // Set color scale
-    //   scale: ["#ffffb2", "#b10026"],
+    // Set color scale
+    scale: ["#ffffb2", "#b10026"],
   
-    //   // Number of breaks in step range
-    //   steps: 10,
+    // Number of breaks in step range
+    steps: 10,
   
-    //   // q for quartile, e for equidistant, k for k-means
-    //   mode: "q",
-    //   style: {
-    //     // Border color
-    //     color: "#fff",
-    //     weight: 1,
-    //     fillOpacity: 0.8
-    //   },
+    // q for quartile, e for equidistant, k for k-means
+    mode: "q",
+    style: {
+    // Border color
+    color: "#fff",
+    weight: 1,
+    fillOpacity: 0.8
+       },
   
-    //   // Binding a pop-up to each layer
-    //   onEachFeature: function(feature, layer) {
+    // Binding a pop-up to each layer
+    //onEachFeature: function(feature, layer) {
     //     layer.bindPopup("Cases: " + feature.Canadian_Cases);
     //   }
-    // }).addTo(myMap);
+    }).addTo(myMap);
   
-    // // Set up the legend
-    // var legend = L.control({ position: "bottomright" });
-    // legend.onAdd = function() {
-    //   var div = L.DomUtil.create("div", "info legend");
-    //   var limits = geojson.options.limits;
-    //   var colors = geojson.options.colors;
-    //   var labels = [];
+    // Set up the legend
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = geojson.options.limits;
+    var colors = geojson.options.colors;
+    var labels = [];
   
-    //   // Add min & max
-    //   var legendInfo = "<h1>Canadian Cases</h1>" +
-    //     "<div class=\"labels\">" +
-    //       "<div class=\"min\">" + limits[0] + "</div>" +
-    //       "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-    //     "</div>";
+    // Add min & max
+    var legendInfo = "<h1>Canadian Cases</h1>" +
+    "<div class=\"labels\">" +
+    "<div class=\"min\">" + limits[0] + "</div>" +
+    "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+     "</div>";
   
-    //   div.innerHTML = legendInfo;
+    div.innerHTML = legendInfo;
   
-    //   limits.forEach(function(limit, index) {
-    //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    //   });
+    limits.forEach(function(limit, index) {
+    labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    });
   
-    //   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    //   return div;
-    // };
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+    };
   
     // // Adding legend to the map
-    // legend.addTo(myMap);
+    legend.addTo(myMap);
   
   });
   
