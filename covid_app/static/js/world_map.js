a05f5976d6d207426b4e9cbfc743fd1f26d57b0e
Heather Dietrich <71471631+HDietrich416@users.noreply.github.com>
2021-02-06 10:18:01 -0500

    added world map

---
+++ covid_app/static/js/world_map.js
@@ -0,0 +1,15 @@
+// Creating map object
+var myMap = L.map("global_map", {
+    center: [40.52, 34.34],
+    zoom: 1.4
+  });
+  
+  // Adding tile layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v9",
+    accessToken: API_KEY
+  }).addTo(myMap);
