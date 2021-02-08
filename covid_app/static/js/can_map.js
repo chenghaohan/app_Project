4841e03cf4f33522245a9e90e7b602565c11e8e2
Heather Dietrich <71471631+HDietrich416@users.noreply.github.com>
2021-02-08 12:10:13 -0500

    included documentation

--- covid_app/static/js/can_map.js
+++ covid_app/static/js/can_map.js
@@ -4,7 +4,7 @@
       zoom: 3.2
     });
   
-  // Adding tile layer
+// Adding tile layer
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -13,24 +13,26 @@
     id: "mapbox/light-v9",
     accessToken: API_KEY
   }).addTo(canMap);
-  
+
+// Adding a popup on each province
+
 function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
-    //console.log(feature, layer)
+
     if (feature.properties && feature.properties.PRENAME) {
         layer.bindPopup("<h3>" + feature.properties.PRENAME + "</h3><hr><p> Cases: </p><p> Deaths:</p><p> Vaccines Administered: </p>");
     }
 }
 
+//GeoJson data for coordinates outlining Provinces
 
 d3.json("/api/main/canmap").then(function(dataset) {
-  //createFeatures(dataset.features);
-  //console.log(dataset);
+
   L.geoJSON(dataset, {
     onEachFeature: onEachFeature
   }).addTo(canMap)    
 });
 
+// App route with total cases, deaths, and vaccines to output into HTML at top of dashboard
 
 d3.json("/api/main/cancovid").then (function(data) {
 
@@ -44,75 +46,16 @@ d3.json("/api/main/cancovid").then (function(data) {
 });
 
 
-  // Create a new choropleth layer
-
-// Load in geojson data
-//var geoData = "/api/main/cancovid";
   
-  // Grab data with d3
+// Grab provincial covid data (cases, deaths, vaccines) with d3
 d3.json("/api/main/provcovid").then (function(data) {
 
-  console.log(data.Ontario)
+  var province = data.map(d =>d.Province);
+  var cases = data.map(d =>d.cases);
+  var deaths = data.map(d =>d.deaths);
+  var vaccinces = data.map(d =>d.vaccines);
+
+  console.log(province)
 
 });
 
-// // Grab data with d3
-// d3.json(geoData, function(data) {
-
-//   // Create a new choropleth layer
-//   geojson = L.choropleth(data, {
-
-//     // Define what  property in the features to use
-//     valueProperty: "MHI2016",
-
-//     // Set color scale
-//     scale: ["#ffffb2", "#b10026"],
-
-//     // Number of breaks in step range
-//     steps: 10,
-
-//     // q for quartile, e for equidistant, k for k-means
-//     mode: "q",
-//     style: {
-//       // Border color
-//       color: "#fff",
-//       weight: 1,
-//       fillOpacity: 0.8
-//     },
-
-//     // Binding a pop-up to each layer
-//     onEachFeature: function(feature, layer) {
-//       layer.bindPopup("Zip Code: " + feature.properties.ZIP + "<br>Median Household Income:<br>" +
-//         "$" + feature.properties.MHI2016);
-//     }
-//   }).addTo(myMap);
-
-//   // Set up the legend
-//   var legend = L.control({ position: "bottomright" });
-//   legend.onAdd = function() {
-//     var div = L.DomUtil.create("div", "info legend");
-//     var limits = geojson.options.limits;
-//     var colors = geojson.options.colors;
-//     var labels = [];
-
-//     // Add min & max
-//     var legendInfo = "<h1>Median Income</h1>" +
-//       "<div class=\"labels\">" +
-//         "<div class=\"min\">" + limits[0] + "</div>" +
-//         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-//       "</div>";
-
-//     div.innerHTML = legendInfo;
-
-//     limits.forEach(function(limit, index) {
-//       labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-//     });
-
-//     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-//     return div;
-//   };
-
-//   // Adding legend to the map
-//   legend.addTo(myMap);
-
-// });
